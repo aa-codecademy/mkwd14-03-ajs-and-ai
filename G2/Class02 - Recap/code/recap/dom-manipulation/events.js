8b0f47203553f3b49c688886d044d216b50e07f5
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2026-02-02 20:36:03 +0100

    Class 02

--- G2/Class02 - Recap/code/recap/dom-manipulation/events.js
+++ G2/Class02 - Recap/code/recap/dom-manipulation/events.js
@@ -1,2 +1,27 @@
 console.log("");
 console.log("===================== EVENTS =====================");
+
+const buttonOne = document.getElementById("btn-one");
+const buttonTwo = document.getElementById("btn-two");
+const buttonThree = document.getElementById("btn-three");
+
+
+buttonOne.addEventListener("click", function () {
+    console.log("Button 1 is clicked!");
+});
+
+buttonTwo.addEventListener("click", function (event) {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.id);
+    console.log(event.target.dataset);
+
+})
+
+function handleClick(event) {
+    console.log("Button 3 has been clicked!");
+    console.log("This is the event:");
+    console.log(event);
+}
+
+buttonThree.addEventListener("click", handleClick);
