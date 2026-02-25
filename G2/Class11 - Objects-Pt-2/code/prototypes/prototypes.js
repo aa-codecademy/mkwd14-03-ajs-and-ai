23df861c9e2382126f45fc40f26c5b54fabd41c1
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2026-02-25 20:06:53 +0100

    Class 11

---
+++ G2/Class11 - Objects-Pt-2/code/prototypes/prototypes.js
@@ -0,0 +1,94 @@
+console.log("================== PROTOTYPES ==================");
+
+// ===> Example with Object.create();
+
+// => Parent object
+const animal = {
+    type: "Mamal",
+    eat: function () {
+        console.log("Nom Nom Nom");
+    }
+}
+
+// => Creating new object that inherits from 'animal'
+const dog = Object.create(animal);
+console.log(dog);
+console.log(dog.type); // Mammal - Inherited from 'animal'
+
+// Adding a new property to 'dog'
+dog.breed = "German Shepard";  // 'dog' object now has its own 'breed' property
+
+dog.eat(); // Inherited method
+
+
+
+console.log("");
+console.log("========== __proto__ ==========");
+// The __proto__ property is a reference to the object's prototype
+// It allows access to properties and methods from the prototype chain
+
+// ===> The Prototype Chain
+// Every object in JavaScript has a prototype
+// If a property or method is not found on the object, JavaScript looks up the prototype chain
+console.log(dog.__proto__); // the 'animal' object
+console.log(dog.__proto__.type); // Mamal
+console.log(dog.__proto__.__proto__); // Object
+console.log(dog.__proto__.__proto__.__proto__); // null
+// console.log(dog.__proto__.__proto__.__proto__.__proto__); // ERROR
+
+console.log(dog.prototype);  // undefined, only constructor functions (and classes) contain the 'prototype' property
+
+console.log(dog.street)
+
+
+
+console.log("");
+console.log("========== prototype ==========");
+// The __proto__ property is a reference to the object's prototype.
+// It allows access to properties and methods from the prototype chain.
+
+function Animal(name) {
+    // Each instance of Animal will have its own 'name' property
+    this.name = name;
+    // This method is assigned directly to the instance, meaning each new Animal object will have its own copy of the 'eat' function (not shared across instances)
+    this.eat = () => console.log(`${this.name} is eating`);
+}
+
+const animalDog = new Animal("Dog");
+const animalRabbit = new Animal("Rabbit");
+
+animalDog.eat();
+animalRabbit.eat();
+
+console.log(Animal.prototype);
+
+Animal.prototype.makeSound = function () {
+    console.log(`The ${this.name} is making a sound !`);
+}
+
+animalDog.makeSound();
+
+
+
+console.clear();
+console.log("");
+console.log("========== Object.setPrototypeOf ==========");
+
+const swimAbility = {
+    isGoodSwimmer: false,
+    swim: function () {
+        console.log(`${this.name} is swimming. It is a ${this.isGoodSwimmer ? "good" : "bad"} swimmer`);
+    }
+}
+
+
+const turtle = new Animal("Turtle");
+// Object.setPrototypeOf(turtle, swimAbility);
+Object.setPrototypeOf(swimAbility, Animal.prototype);
+Object.setPrototypeOf(turtle, swimAbility);
+
+turtle.eat();
+turtle.makeSound();
+turtle.swim();
+
+console.log(Object.getPrototypeOf(turtle));
